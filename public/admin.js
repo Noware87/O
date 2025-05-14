dac50b2eaf3d0e841c091b5a3f9335dbd4884499
Noware87 <nima_r1@hotmail.com>
2025-05-14 14:30:44 +0200

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,7 +1,6 @@
-```js
+
 (async()=>{
   const res=await fetch('/api/admin/history');
   const data=await res.json();
   document.getElementById('history').innerText=JSON.stringify(data,null,2);
 })();
-```
